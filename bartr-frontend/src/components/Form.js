d6f10519826b402e32704f0ba5ca637707ae9c8f
Ian Marshall <imarshall14@gmail.com>
2021-05-17 14:03:04 -0400

    create basic controlled form for address

---
+++ bartr-frontend/src/components/Form.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+
+    state = {
+        name: '',
+        streetAddress: '',
+        city: '',
+        state: '',
+        areaCode: ''
+    }
+
+    handleChange = (e) => {}
+
+    render(){
+        return(
+            <div>
+                <form>
+                    <label>Name</label>
+                    <input type='text' placeholder='Name' value = {this.state.name} onChange={this.handleChange}></input>
+                    <label>Street address</label>
+                    <input type='text' placeholder='Street Address' value = {this.state.streetAddress} onChange={this.handleChange}></input>
+                    <label>City</label>
+                    <input type='text' placeholder='City' value = {this.state.city} onChange={this.handleChange}></input>
+                    <label>State</label>
+                    <input type='text' placeholder='State' value = {this.state.state} onChange={this.handleChange}></input>
+                    <label>Area Code</label>
+                    <input type='text' placeholder='Area Code' value = {this.state.areaCode} onChange={this.handleChange}></input>
+                </form>
+            </div>
+        )
+    }
+
+}
--- bartr-frontend/src/components/Products.js
+++ bartr-frontend/src/components/Products.js
@@ -4,16 +4,6 @@ import Product from './Product.js'
 import './products-stylesheet.css'
 
 
-// const products = [
-//     { id: 1, name: `insulin`, description: `the good stuff`, image: 'https://image.flaticon.com/icons/png/512/1914/1914515.png', price: 'everything at a price'},
-//     { id: 2, name: `monkey's paw`, description: `three more things, what could go wrong?`, image: 'https://www.pngitem.com/pimgs/m/119-1194045_monkey-paw-png-transparent-png.png', price: 'everything at a price'},
-//     { id: 3, name: `insulin again`, description: `the good stuff`, image: 'https://image.flaticon.com/icons/png/512/1914/1914515.png', price: 'everything at a price'},
-//     { id: 4, name: `more insulin`, description: `the good stuff`, image: 'https://image.flaticon.com/icons/png/512/1914/1914515.png', price: 'everything at a price'},
-//     { id: 5, name: `insulin for days`, description: `the good stuff`, image: 'https://image.flaticon.com/icons/png/512/1914/1914515.png', price: 'everything at a price'},
-//     { id: 6, name: `even more insulin`, description: `the good stuff`, image: 'https://image.flaticon.com/icons/png/512/1914/1914515.png', price: 'everything at a price'}
-// ];
-
-
 const Products = (props) => {
     return(
         <main className="container">
