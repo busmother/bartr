d80cda50f4ead89e93b874f199b04e9c63248f84
Ian Marshall <imarshall14@gmail.com>
2021-05-26 11:02:31 -0400

    update itemReducer with REMOVE_ITEM case

--- bartr-frontend/src/reducers/itemReducer.js
+++ bartr-frontend/src/reducers/itemReducer.js
@@ -3,8 +3,16 @@ export default function itemReducer(state = {items: []}, action) {
     switch (action.type) {
         case 'ADD_ITEM':
             return {...state, items: [...state.items, action.payload]}
-        // case 'REMOVE_ITEM':
-        //     return {...state, items: [...state.items]} //this needs more
+        case 'REMOVE_ITEM':
+            const removalIndex = state.items.findIndex(item => item.id === action.id);
+            return (
+                {...state, 
+                    items: [
+                        ...state.items.slice(0, removalIndex),
+                    ...state.items.slice(removalIndex + 1)
+                    ]
+                }
+            )
         default:
             return state
     }
--- bartr-frontend/src/reducers/orderReducer.js
+++ bartr-frontend/src/reducers/orderReducer.js
@@ -5,14 +5,15 @@ export default function orderReducer(state = {orders: []}, action) {
             return {...state, orders: [...state.orders, action.payload]}
         case 'REMOVE_ORDER':
             const removalIndex = state.orders.findIndex(order => order.id === action.id);
-            return {...state, orders: [
-                ...state.orders.slice(0, removalIndex),
-                ...state.orders.slice(removalIndex + 1)
-            ]}
+            return (
+                {...state, 
+                    orders: [
+                    ...state.orders.slice(0, removalIndex),
+                    ...state.orders.slice(removalIndex + 1)
+                    ]
+                }
+            )
         default:
             return state
     }
 }
-
-//is there a reason there's not one reducer with different cases?
-//is the state here the same as the state in the productReducer?
