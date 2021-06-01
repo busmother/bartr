57cd9797dc1e740844c9d729b2445ce2cc9dac5f
Ian Marshall <imarshall14@gmail.com>
2021-06-01 17:28:43 -0400

    add controlled form to Login component

--- bartr-frontend/src/components/Login.js
+++ bartr-frontend/src/components/Login.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import {connect} from 'react'
 export default class Login extends Component {
+
+    state = {username: ""};
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.login(this.state)
+    }
 
     render () {
         return(
             <div>
-                Login!
+                <h3>Login:</h3>
+                <form onSubmit = {this.handleSubmit}>
+                    <label for = "username">Username:</label>
+                    <input
+                        onChange = {this.handleChange}
+                        id = "username"
+                        name = "username"
+                        value = {this.state.username}
+                    />
+                    <input type = "submit" value = "Sign in" />
+                </form>
             </div>
-        )
+        );
     }
 }
