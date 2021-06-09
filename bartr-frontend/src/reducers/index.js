d0e4fbdddc0fc89cfc1917c66585329da3bb1c2b
Ian Marshall <imarshall14@gmail.com>
2021-06-09 11:10:54 -0400

    add popupreducer to reducers/index

--- bartr-frontend/src/reducers/index.js
+++ bartr-frontend/src/reducers/index.js
@@ -3,10 +3,12 @@ import productReducer from './productReducer'
 import orderReducer from './orderReducer'
 import itemReducer from './itemReducer'
 import user from './userReducer'
+import popUpReducer from './popUpReducer'
 
 export default combineReducers({
     productReducer,
     orderReducer,
     itemReducer,
-    user
+    user,
+    popUpReducer
 })
