09b396e07e4585d3ee094bba3dd981bab2b46cdc
Ian Marshall <imarshall14@gmail.com>
2021-06-09 16:14:01 -0400

    set up OrdersContainer, Orders, and Order to render info for past orders

--- bartr-frontend/src/components/Order.js
+++ bartr-frontend/src/components/Order.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import Moment from 'moment'
 
 const Order = ({ order }) => {
     return (
-        console.log("order.data from Order", order.data)
+        
         <div>
-            {/* <h5>Order Number: {order.data.id}</h5>
+            <h5>Order number: #{order.id}, {order.attributes.updated_at} </h5>
+            <h5>Order info: </h5>
+            <h5>{order.attributes.recipient}<br></br>
+            {order.attributes.street_address}<br></br>
+            {order.attributes.city}, {order.attributes.state} {order.attributes.zip_code}</h5>
             <ul>
-                {order.data.attributes.items.map((item) => (
+                {order.attributes.items.map((item) => (
                     <li>{item.name} - {item.price}</li>
                 ))}
-            </ul> */}
+            </ul>
             <h5>"Order total:</h5>
         </div>
     )
--- bartr-frontend/src/components/Orders.js
+++ bartr-frontend/src/components/Orders.js
@@ -6,13 +6,14 @@ const Orders = (props) => {
     return(
         <div>
             <ul>
-            {props.orders &&
-            props.orders.map((order) => (
-                <li><Order key={order.id} className = "order" /></li>
-            ))}
+                {props.orders &&
+                    props.orders.map((order) => (
+                        <li><Order key={order.id} order={order} order_id={order.id} className = "order" /></li>
+                    ))}
             </ul>
         </div>
     )
 }
 
 export default Orders
+
