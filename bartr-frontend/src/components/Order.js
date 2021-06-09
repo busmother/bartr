ce061889b925f88793dcccd29a5ac015bbe09731
Ian Marshall <imarshall14@gmail.com>
2021-06-09 16:35:52 -0400

    install moment and format order date

--- bartr-frontend/src/components/Order.js
+++ bartr-frontend/src/components/Order.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Moment from 'moment'
+import {moment} from 'moment'
 
 const Order = ({ order }) => {
     return (
         
         <div>
-            <h5>Order number: #{order.id}, {order.attributes.updated_at} </h5>
+            <h5>Order number: #{order.id}, {moment(order.attributes.updated_at).format('MMMM Do YYYY, h:mm:ss a')} </h5>
             <h5>Order info: </h5>
             <h5>{order.attributes.recipient}<br></br>
             {order.attributes.street_address}<br></br>
             {order.attributes.city}, {order.attributes.state} {order.attributes.zip_code}</h5>
             <ul>
-                {order.attributes.items.map((item) => (
-                    <li>{item.name} - {item.price}</li>
+                {order.attributes.products.map((product) => (
+                    <li className="product-list">{product.name} - {product.price}</li>
                 ))}
             </ul>
-            <h5>"Order total:</h5>
+            <h5>Order total:</h5>
         </div>
     )
 
