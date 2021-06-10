d552247bdcd4b564d1aedef90d5e6f4ce24f326b
Ian Marshall <imarshall14@gmail.com>
2021-06-09 21:17:05 -0400

    add FETCH_ITEMS action to itemReducer

--- bartr-frontend/src/reducers/itemReducer.js
+++ bartr-frontend/src/reducers/itemReducer.js
@@ -1,6 +1,8 @@
 export default function itemReducer(state = {items: []}, action) {
 
     switch (action.type) {
+        case 'FETCH_ITEMS':
+            return {items: action.payload}
         case 'ADD_ITEM':
             return {...state, items: [...state.items, action.payload]}
         case 'REMOVE_ITEM':
