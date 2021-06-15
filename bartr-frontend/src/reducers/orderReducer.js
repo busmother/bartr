5e72a85645e8dbc4f98b6640fa90eea97a648c94
Ian Marshall <imarshall14@gmail.com>
2021-06-15 17:25:05 -0400

    add FETCH_ORDER_TOTAL case to orderReducer

--- bartr-frontend/src/reducers/orderReducer.js
+++ bartr-frontend/src/reducers/orderReducer.js
@@ -3,6 +3,8 @@ export default function orderReducer(state = {orders: []}, action) {
     switch (action.type) {
         case 'FETCH_ORDERS':
             return {orders: action.payload}
+        case 'FETCH_ORDER_TOTAL':
+            return {open_order_id: action.payload}
         case 'ADD_ORDER':
             return {...state, orders: [...state.orders, action.payload]}
         case 'ClEAR_ORDERs': 
