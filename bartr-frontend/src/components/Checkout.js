ba9e0f7e77112d429793a06664894aed42cda37c
Ian Marshall <imarshall14@gmail.com>
2021-06-17 16:20:20 -0400

    add basic validations to form

--- bartr-frontend/src/components/Checkout.js
+++ bartr-frontend/src/components/Checkout.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { closeOrder, newOrder } from '../actions/order'
+import { validateFields } from './Validator'
 
 
 class Checkout extends Component {
 
     state = {
-        recipient: '',
-        streetAddress: '',
-        city: '',
-        state: '',
-        zipCode: ''
-    }
+        recipient: {
+            value: '',
+            validateOnChange: false,
+            error: ''
+        },
+        streetAddress: {
+            value: '',
+            validateOnChange: false,
+            error: ''
+        },
+        city: {
+            value: '',
+            validateOnChange: false,
+            error: ''
+        },
+        state: {
+            value: '',
+            validateOnChange: false,
+            error: ''
+        },
+        zipCode: {
+            value: '',
+            validateOnChange: false,
+            error: ''
+        },
+        submitCalled: false,
+        allFieldsValidated: false
+    };
 
     handleChange = (event) => {
         this.setState({
@@ -22,37 +45,60 @@ class Checkout extends Component {
     handleSubmit = (event) => {
         event.preventDefault()
         this.props.closeOrder(this.state, this.props.user_id, this.props.order_id)
-        // this.props.newOrder(this.props.user_id)
     }
-
-            // this.setState({
-        //     recipient: '',
-        //     streetAddress: '',
-        //     city: '',
-        //     state: '',
-        //     zipCode: ''
-        // })
 
     render(props){
         console.log("this.props from Checkout", this.props)
+        const { recipient, streetAddress, city, state, zipCode, allFieldsValidated } = this.state;
         return(
             <div>
                 <br></br><br></br>
                 Confirm shipping information for your current order of ${this?.props?.order_total}:
                 <br></br><br></br><br></br><br></br>
-                <form onSubmit={this.handleSubmit}>
+                <div className = "card-body">
+                    {allFieldsValidated && (
+                        <p className="text-success text-center">
+                            Success, All fields are validated
+                        </p>
+                    )}
+                <form onSubmit={this.handleSubmit}> {/* {evt => this.handleSubmit(evt)} */}
                     <label>Recipient:</label>
-                    <input type='text' placeholder='Recipient' value = {this.state.recipient} name='recipient' onChange={this.handleChange}/><br></br><br></br>
+                    <input 
+                        type='text' 
+                        name='recipient' 
+                        value = {recipient.value}  
+                        placeholder='Recipient' 
+                        className={classnames(
+                            'form-control',
+                            { 'is-valid': recipient.error === false },
+                            { 'is-invalid': recipient.error}
+                        )}
+                        onChange={e => 
+                            this.handleChange(validateFields.validateRecipient, e)
+                        }
+                        onBlur={e =>
+                        this.handleBlur(validateFields.validateEmail, e)}
+                    />
+                    <br></br><br></br>
                     <label>Street address:</label>
-                    <input type='text' placeholder='Street Address' value = {this.state.streetAddress} name='streetAddress' onChange={this.handleChange}/><br></br><br></br>
+                    <input type='text' placeholder='Street Address' value = {this.state.streetAddress} name='streetAddress' onChange={this.handleChange}/
+                    ><br></br><br></br>
                     <label>City:</label>
                     <input type='text' placeholder='City' value = {this.state.city} name='city' onChange={this.handleChange}/>
                     <label>   State:</label>
                     <input type='text' placeholder='State' value = {this.state.state} name='state' onChange={this.handleChange}/>
                     <label>   Area Code:</label>
-                    <input type='text' placeholder='Area Code' value = {this.state.zipCode} name='zipCode' onChange={this.handleChange}/><br></br><br></br><br></br>
-                    <input type='submit'></input>
+                    <input type='text' placeholder='Area Code' value = {this.state.zipCode} name='zipCode' onChange={this.handleChange}/>
+                    <br></br><br></br><br></br>
+                    <button
+                        type="submit"
+                        className="btn btn-secondary btn-block"
+                        onMouseDown={() => this.setState({ submitCalled: true })}
+                    >
+                        Submit Order
+                    </button>
                 </form>
+                </div>
             </div>
         )
     }
