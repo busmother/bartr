c575c89110937406970af93a2a34b6c3734de15a
Ian Marshall <imarshall14@gmail.com>
2021-09-21 11:57:43 -0400

    add basic fetch to ResultsContainer

--- Development/nasa-image-search/src/containers/ResultsContainer.js
+++ Development/nasa-image-search/src/containers/ResultsContainer.js
@@ -6,11 +6,25 @@ class ResultsContainer extends Component {
         images: []
     }
 
+    https://api.nasa.gov/planetary/apod?api_key=VodNvDXzlgg3cT0dgOn9JuFzkcQLdDhEhfbwinEI&moon
+
+    const request = new XMLHttpRequest();
+    const url = "https://api.nasa.gov/planetary/apod?api_key=";
+    const apiKey = "VodNvDXzlgg3cT0dgOn9JuFzkcQLdDhEhfbwinEI";
+
+    fetchImages = (query = "kacey musgraves", searchType = "concept_tags") => {
+        fetch(`${url+apiKey}+"&"+${searchType+query}`)
+        .then(res => res.json())
+        .then(({data}) => {
+            this.setState({ images: data.map( image => ({ url: image.url }))})
+        })
+    }
+
     render(){
         return (
             <div>
                 <ImageSearch fetchImages={this.fetchImages} />
-                <Results images={this.state.images}
+                <Results images={this.state.images} />
             </div>
         )
     }
